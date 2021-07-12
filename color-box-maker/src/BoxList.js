8dde310a5cd98051464dea27bfaf2b7a9bb44f99
brandon mcintosh <brandon.l.mcintosh@gmail.com>
2021-07-12 15:35:27 -0700

    init

---
+++ color-box-maker/src/BoxList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BoxList = () => {
+
+};
+
+export default BoxList;
---
+++ todo-app/src/TodoList.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Todo from "./Todo";
+import NewTodoForm from "./NewTodoForm";
+
+const TodoList = () => {
+
+};
+
+export default TodoList;
